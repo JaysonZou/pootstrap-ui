175d2bdaf3267485a8a719bcbeb8be22561a27d3
ycescape <1120957260@qq.com>
2022-03-03 13:01:43 +0800

    支持scss

--- src/lib/input/index.tsx
+++ src/lib/input/index.tsx
@@ -1,8 +1,9 @@
 import { InputHTMLAttributes } from "react"
 import './index.css'
+import './input.scss'
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 const Input: React.FunctionComponent<InputProps> = (props) => {
-  return <input className={`ele-input`} {...props}/>
+  return <input className={`ele-input input`} {...props}/>
 }
 export default Input
